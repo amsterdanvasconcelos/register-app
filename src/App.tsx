009fec9196cb83fe0de8b9a839247d9f5d5fa1e4
Amsterdan Vasconcelos <amsterdan.sci@gmail.com>
2024-08-09 22:39:25 -0300

    feat: create light theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
+import { Button, ThemeProvider } from '@mui/material';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { lightTheme } from './shared/themes';
+
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <ThemeProvider theme={lightTheme}>
+        <BrowserRouter>
+          <Routes>
+            <Route
+              path="/pagina-inicial"
+              element={
+                <Button variant="contained" color="primary">
+                  Teste Material UI
+                </Button>
+              }
+            />
+            <Route path="*" element={<Navigate to={'/pagina-inicial'} />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
+    </>
+  );
 };
 
 export default App;
